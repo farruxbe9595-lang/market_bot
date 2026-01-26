7bb421613958bfd613d7125a5bef5699ae921a0e
farruxbe9595-lang <farruxbe9595@gmail.com>
2026-01-26 12:17:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
-from handlers.group import router as group_router
-from handlers.private import router as private_router
+from handlers.group import register_group
 
 async def main():
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
 
-    dp.include_router(group_router)
-    dp.include_router(private_router)
+    # GROUP handlerlarni ulaymiz
+    await register_group(dp, bot)
 
     await dp.start_polling(bot)
 
