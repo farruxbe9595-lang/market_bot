04520678c16998d3ab366db32405a209575b55ea
farruxbe9595-lang <farruxbe9595@gmail.com>
2026-01-26 12:25:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, BOT_USERNAME
 from handlers.group import register_group
 
 async def main():
