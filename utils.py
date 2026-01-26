8a0de2aabd17236d2bcca5eb8588c6ed79f790f4
farruxbe9595-lang <farruxbe9595@gmail.com>
2026-01-26 12:18:59 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,3 @@
-from aiogram import Bot
-
-async def is_admin(bot: Bot, chat_id: int, user_id: int) -> bool:
-    try:
-        member = await bot.get_chat_member(chat_id, user_id)
-        return member.status in ("administrator", "creator")
-    except Exception:
-        return False
+async def is_admin(bot, chat_id, user_id):
+    member = await bot.get_chat_member(chat_id, user_id)
+    return member.status in ("administrator", "creator")
