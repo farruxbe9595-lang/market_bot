1be7871c7487f6a875a70c03c470d90c622b68d4
farruxbe9595-lang <farruxbe9595@gmail.com>
2026-01-26 11:12:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
 
+# Bot token (Railway Variables’da bo‘lishi shart)
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-BOT_USERNAME = "Buyurtma9020_bot"   # @siz yoziladi
+# Bot username (@siz yoziladi)
+BOT_USERNAME = "Buyurtma9020_bot"
 
+# Market group ID (asosiy savdo guruhi)
 MARKET_GROUP_ID = -1001234567890
+
+# Muhokama (discussion) topic ID
 DISCUSSION_TOPIC_ID = 1
+
+# Adminlar buyurtma keladigan kanal / guruh ID
+ADMIN_CHANNEL_ID = -1009876543210
+
+# (hozircha ishlatilmasa ham, import xatosini yopish uchun)
+ORDER_TIMEOUT = 300  # sekund (5 daqiqa)
