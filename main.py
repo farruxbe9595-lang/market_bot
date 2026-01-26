f45544c3b938ab6771e45c3dbbd3650fa106d264
farruxbe9595-lang <farruxbe9595@gmail.com>
2026-01-26 10:42:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import asyncio
 from aiogram import Bot, Dispatcher
+import asyncio
 from config import BOT_TOKEN
 from handlers.group import register_group
 from handlers.private import register_private
+
+BOT_USERNAME = "Buyurtma9020_bot"
 
 async def main():
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
 
-    await register_group(dp)
+    await register_group(dp, bot, BOT_USERNAME)
     await register_private(dp, bot)
 
-   
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
