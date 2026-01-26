38029bd48a3a74290d0fa56e0b7d5d8fac47ee0a
farruxbe9595-lang <farruxbe9595@gmail.com>
2026-01-26 11:07:08 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,8 @@
-import time
 from aiogram import Bot
-from config import ORDER_TIMEOUT
-from storage import ORDERS
 
 async def is_admin(bot: Bot, chat_id: int, user_id: int) -> bool:
-    admins = await bot.get_chat_administrators(chat_id)
-    return any(a.user.id == user_id for a in admins)
-
-def update_order_time(user_id: int):
-    if user_id in ORDERS:
-        ORDERS[user_id]["last_update"] = time.time()
+    try:
+        member = await bot.get_chat_member(chat_id, user_id)
+        return member.status in ("administrator", "creator")
+    except Exception:
+        return False
