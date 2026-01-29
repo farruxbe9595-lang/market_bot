7ca13f0dcf36c442ab6fee6caabc66afdeb6d79b
farruxbe9595-lang <farruxbe9595@gmail.com>
2026-01-29 14:28:14 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,33 +1,8 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
-def admin_cancel_kb() -> InlineKeyboardMarkup:
-    """
-    Admin uchun: mahsulot qo‘shishni bekor qilish tugmasi
-    """
+def admin_cancel_kb():
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="❌ Bekor qilish",
-                    callback_data="cancel_add"
-                )
-            ]
-        ]
-    )
-
-
-def order_button(bot_username: str, product_id: str) -> InlineKeyboardMarkup:
-    """
-    Mahsulot ostida chiqadigan: Buyurtma berish tugmasi
-    """
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="🛒 Buyurtma berish",
-                    url=f"https://t.me/{bot_username}?start=order_{product_id}"
-                )
-            ]
+            [InlineKeyboardButton(text="❌ Bekor qilish", callback_data="cancel_add")]
         ]
     )
