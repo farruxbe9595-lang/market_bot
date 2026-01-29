7928ec4fa33feb72af542d593350b3b5e940b36e
farruxbe9595-lang <farruxbe9595@gmail.com>
2026-01-29 16:26:02 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,7 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# @siz yoziladi
 BOT_USERNAME = "Buyurtma9020_bot"
 
-# MARKET group ID
 MARKET_GROUP_ID = -1003618675735
-
-# Umumiy chat topic ID (masalan: Nuhokama)
-DISCUSSION_TOPIC_ID = 1
+ADMIN_CHANNEL_ID = -1003631320685
