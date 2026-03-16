90464c1e78a0f2b284037e82eca57ed1eb2c08f9
farruxbe9595-lang <farruxbe9595@gmail.com>
2026-03-16 11:15:09 +0500

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def admin_cancel_kb():
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="❌ Bekor qilish", callback_data="cancel_add")]
-        ]
-    )
